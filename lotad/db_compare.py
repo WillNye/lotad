73b4afb29f6837ad3e3052a1061a371a55f26c27
WillNye <willbeas88@gmail.com>
2025-01-07 18:45:01 -0600

    compare_table_data now runs in a proc pool for a notable improvement in performance. Added additional methods to DatabaseComparator for additional context when running compare_all.

--- lotad/db_compare.py
+++ lotad/db_compare.py
@@ -1,24 +1,20 @@
-import logging
-import os
+import multiprocessing
 import re
-from datetime import datetime
-from typing import Optional
+from typing import Union
 
 import duckdb
 
-from lotad.data_analysis import DriftAnalysis
-from lotad.utils import get_row_hash
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-CPU_COUNT = os.cpu_count()
+from lotad.config import Config, TableRules, TableRuleType, CPU_COUNT
+from lotad.connection import LotadConnectionInterface
+from lotad.data_analysis import DriftAnalysis, MissingTableDrift, TableDataDiff, TableSchemaDrift
+from lotad.logger import logger
 
 
 class DatabaseComparator:
-    """Compares two DuckDB databases to identify schema and data differences.
+    """Compares two databases to identify schema and data differences.
 
     This class provides functionality to perform comprehensive comparisons between
-    two DuckDB databases, analyzing differences in table structures, schemas, and
+    two databases, analyzing differences in table structures, schemas, and
     data content. It supports detailed analysis of table presence, column
     definitions, data types, and row-level differences.
 
@@ -26,49 +22,24 @@ class DatabaseComparator:
     identified differences for further analysis and reporting.
 
     Attributes:
-        db1_path (str): File path to the first DuckDB database.
-        db1 (duckdb.DuckDBPyConnection): Connection to the first database.
-        db2_path (str): File path to the second DuckDB database.
-        db2 (duckdb.DuckDBPyConnection): Connection to the second database.
+        db1_path (str): Connection string to the first database.
+        db2_path (str): Connection string to the second database.
         drift_analysis (DriftAnalysis): Instance for tracking and storing comparison results.
     """
 
-    def __init__(self, db1_path: str, db2_path: str):
-        """Initialize connections to both DuckDB databases."""
-        self.db1_path = str(db1_path)
-        self.db1 = duckdb.connect(self.db1_path, read_only=True)
-        self.db1.create_function("get_row_hash", get_row_hash)
+    def __init__(self, config: Config):
+        self.config = config
+        self.db1_path = str(config.db1_connection_string)
+        self.db2_path = str(config.db2_connection_string)
+        self.db_interface: LotadConnectionInterface = LotadConnectionInterface.create_connection()
 
-        self.db2_path = str(db2_path)
-        self.db2 = duckdb.connect(self.db2_path, read_only=True)
-        self.db2.create_function("get_row_hash", get_row_hash)
+        self.drift_analysis = DriftAnalysis(self.config)
 
-        self.drift_analysis = DriftAnalysis()
-
-    def get_tables(self, connection: duckdb.DuckDBPyConnection) -> list[str]:
-        """Get list of all tables in a database."""
-        return connection.execute(
-            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main'"
-        ).fetchall()
-
-    def get_schema(self, connection: duckdb.DuckDBPyConnection, table: str) -> dict:
-        """Get schema information for a table."""
-        columns = connection.execute(f"""
-            SELECT column_name, data_type
-            FROM information_schema.columns
-            WHERE table_name = '{table}'
-            AND table_schema = 'main'
-            AND data_type NOT LIKE 'TIMESTAMP%'
-            AND data_type NOT LIKE 'DATE'
-            ORDER BY ordinal_position
-        """).fetchall()
-        return {col[0]: col[1] for col in columns}
-
-    def compare_table_schemas(self, table: str) -> dict:
+    def generate_table_schema_drift(self, table_name: str) -> list[TableSchemaDrift]:
         """Detects schemas differences between the two db files.
 
         Args:
-            table (str): Name of the table to compare.
+            table_name (str): Name of the table_name to compare.
 
         Returns:
             dict: Dictionary containing schema differences, including:
@@ -76,84 +47,86 @@ class DatabaseComparator:
                 - missing_in_db1: Columns present in db2 but not in db1
                 - type_mismatches: Columns with different data types
         """
-        schema1 = self.get_schema(self.db1, table)
-        schema2 = self.get_schema(self.db2, table)
+        db1 = self.db_interface.get_connection(self.db1_path, read_only=True)
+        db2 = self.db_interface.get_connection(self.db2_path, read_only=True)
+        schema1 = self.db_interface.get_schema(db1, table_name)
+        schema2 = self.db_interface.get_schema(db2, table_name)
+        response = []
 
-        return {
-            'missing_in_db2': set(schema1.keys()) - set(schema2.keys()),
-            'missing_in_db1': set(schema2.keys()) - set(schema1.keys()),
-            'type_mismatches': {
-                col: (schema1[col], schema2[col])
-                for col in set(schema1.keys()) & set(schema2.keys())
-                if schema1[col] != schema2[col]
-            }
-        }
+        db1.close()
+        db2.close()
 
-    def compare_table_data(self, table: str) -> dict:
-        """Performs a detailed comparison of table data between the two db files.
+        for column in set(schema1.keys()) - set(schema2.keys()):
+            response.append(
+                TableSchemaDrift(
+                    table_name=table_name,
+                    db1=self.db1_path,
+                    db2=self.db2_path,
+                    column_name=column,
+                    db1_column_type=schema1[column],
+                    db2_column_type=None,
+                )
+            )
 
-        Analyzes row-level differences between the two databases using hash-based
-        comparison. Records differences in the drift analysis system for future
-        reference.
+        for column in set(schema2.keys()) - set(schema1.keys()):
+            response.append(
+                TableSchemaDrift(
+                    table_name=table_name,
+                    db1=self.db1_path,
+                    db2=self.db2_path,
+                    column_name=column,
+                    db1_column_type=None,
+                    db2_column_type=schema2[column],
+                )
+            )
 
-        Args:
-            table (str): Name of the table to compare.
+        for col in set(schema1.keys()) & set(schema2.keys()):
+            if schema1[col] != schema2[col]:
+                response.append(
+                    TableSchemaDrift(
+                        table_name=table_name,
+                        db1=self.db1_path,
+                        db2=self.db2_path,
+                        column_name=col,
+                        db1_column_type=schema1[col],
+                        db2_column_type=schema2[col],
+                    )
+                )
 
-        Returns:
-            dict: Dictionary containing comparison results:
-                - row_count_diff: Difference in number of rows between databases
-                - only_in_db1: DataFrame of rows unique to first database
-                - only_in_db2: DataFrame of rows unique to second database
-        """
+        return response
 
-        start_time = datetime.now()
-        logger.debug(f"Getting table data for {table} on df1")
-        df1 = self.db1.execute(
-            f"SELECT get_row_hash(ROW_TO_JSON(t)::VARCHAR) AS row_hash, * FROM {table} t"
-        ).df()
-        logger.debug(f"Finished table data for {table} on df1 in {datetime.now() - start_time}")
-        logger.debug(f"Getting table data for {table} on df2")
-        df2 = self.db2.execute(
-            f"SELECT get_row_hash(ROW_TO_JSON(t)::VARCHAR) AS row_hash, * FROM {table} t"
-        ).df()
+    @staticmethod
+    def generate_missing_table_drift(
+        db1: str,
+        db1_tables: set[str],
+        db2: str,
+        db2_tables: set[str],
+    ) -> list[MissingTableDrift]:
+        response = []
+        for table_name in db1_tables:
+            if table_name not in db2_tables:
+                response.append(
+                    MissingTableDrift(
+                        table_name=table_name,
+                        observed_in=db1,
+                        missing_in=db2
+                    )
+                )
 
-        # Compare row counts
-        row_diff = len(df1) - len(df2)
+        for table_name in db2_tables:
+            if table_name not in db1_tables:
+                response.append(
+                    MissingTableDrift(
+                        table_name=table_name,
+                        observed_in=db2,
+                        missing_in=db1
+                    )
+                )
 
-        # Find differences using hash comparison
-        only_in_db1 = df1[~df1['row_hash'].isin(df2['row_hash'])]
-        only_in_db2 = df2[~df2['row_hash'].isin(df1['row_hash'])]
+        return response
 
-        self.drift_analysis.add_data_drift(
-            only_in_db1,
-            table,
-            self.db1_path
-        )
-
-        self.drift_analysis.add_data_drift(
-            only_in_db2,
-            table,
-            self.db2_path
-        )
-
-        return {
-            'row_count_diff': row_diff,
-            'only_in_db1': only_in_db1,
-            'only_in_db2': only_in_db2
-        }
-
-    def compare_all(
-        self,
-        ignore_tables: Optional[list[str]] = None,
-        target_tables: Optional[list[str]] = None,
-
-    ) -> dict:
+    def compare_all(self):
         """Performs a comprehensive comparison of all tables between the 2 dbs.
-
-        Args:
-            ignore_tables (Optional[list[str]]): List of table names to exclude from comparison.
-            target_tables (Optional[list[str]]): List of specific tables to compare. If provided,
-                                        only these tables will be compared.
 
         Returns:
             dict: Dictionary containing comparison results:
@@ -164,40 +137,177 @@ class DatabaseComparator:
         respecting the ignore_tables and tables parameters for filtering.
         """
 
-        tables1 = set(table[0] for table in self.get_tables(self.db1))
-        tables2 = set(table[0] for table in self.get_tables(self.db2))
+        db1 = self.db_interface.get_connection(self.db1_path, read_only=True)
+        db2 = self.db_interface.get_connection(self.db2_path, read_only=True)
 
-        results = {
-            'missing_tables': {
-                'in_db2': tables1 - tables2,
-                'in_db1': tables2 - tables1
-            },
-            'common_tables': {}
-        }
+        tables1 = set(table[0] for table in self.db_interface.get_tables(db1))
+        tables2 = set(table[0] for table in self.db_interface.get_tables(db2))
+        all_tables = sorted(tables1 & tables2)
 
-        for table in sorted(tables1 & tables2):
-            table_name = table.lower()
-            if ignore_tables and any(re.match(it, table_name, re.IGNORECASE) for it in ignore_tables):
-                logger.info(f"{table} is an ignored table, skipping.")
+        all_schema_drift = []
+        for table in all_tables:
+            if schema_drift := self.generate_table_schema_drift(table):
+                all_schema_drift.extend(schema_drift)
+        if all_schema_drift:
+            self.drift_analysis.add_schema_drift(all_schema_drift)
+
+        if missing_table_drift := self.generate_missing_table_drift(
+            self.db1_path,
+            tables1,
+            self.db1_path,
+            tables2,
+        ):
+            self.drift_analysis.add_missing_table_drift(missing_table_drift)
+
+        db1.close()
+        db2.close()
+
+        table_data = []
+        ignore_tables = self.config.ignore_tables
+        target_tables = self.config.target_tables
+
+        with multiprocessing.Pool(CPU_COUNT) as pool:
+            results = []
+
+            for table_name in all_tables:
+                if ignore_tables and any(re.match(it, table_name, re.IGNORECASE) for it in ignore_tables):
+                    logger.info(f"{table_name} is an ignored table, skipping.")
+                    continue
+
+                if target_tables and any(re.match(tt, table_name, re.IGNORECASE) for tt in target_tables):
+                    logger.info(f"{table_name} is not a target table, skipping.")
+                    continue
+
+                result = pool.apply_async(compare_table_data, (self.config, table_name))
+                results.append(result)
+
+            # Get the results
+            for result in results:
+                try:
+                    tmp_table = result.get()
+                    if tmp_table:
+                        table_data.append(
+                            tmp_table
+                        )
+                except duckdb.duckdb.CatalogException:
+                    continue
+
+        self.drift_analysis.add_data_drift(table_data)
+
+
+def generate_schema_columns(
+    db_schema: dict,
+    alt_db_schema: dict,
+    table_rules: Union[TableRules, None]
+):
+    db_columns = []
+    for col, col_type in db_schema.items():
+        if table_rules:
+            col_rule = table_rules.get_rule(col)
+            if col_rule and col_rule.rule_type == TableRuleType.IGNORE_COLUMN:
                 continue
+        if col in alt_db_schema and alt_db_schema[col] != col_type:
+            col = f'"{col}"::VARCHAR'
+        else:
+            col = f'"{col}"'
+        db_columns.append(col)
 
-            if target_tables and any(re.match(tt, table_name, re.IGNORECASE) for tt in target_tables):
-                logger.info(f"{table} is not a target table, skipping.")
-                continue
-            logger.info(f"Comparing table {table}")
-            self.compare_table_data(table)
+    for col in alt_db_schema.keys():
+        if col not in db_schema:
+            db_columns.append(f'NULL AS "{col}"')
 
-            # logger.info(f"Comparing table: {table}")
-            # results['common_tables'][table] = {
-            #     'schema_differences': self.compare_table_schemas(table),
-            #     'data_differences': self.compare_table_data(table)
-            # }
+    return db_columns
 
-        return results
 
-    def close(self):
-        self.db1.close()
-        self.db2.close()
+def get_gen_table_query(
+    table: str,
+    config: Config,
+    db1_schema: dict,
+    db2_schema: dict,
+):
+    table_rules = config.get_table_rules(table)
+    db1_path = config.db1_connection_string
+    db2_path = config.db2_connection_string
+    db1_columns = ",\n".join(
+        generate_schema_columns(db1_schema, db2_schema, table_rules)
+    )
+    db2_columns = ",\n".join(
+        generate_schema_columns(db2_schema, db1_schema, table_rules)
+    )
+
+    return f"""
+CREATE OR REPLACE TABLE {table}_t1 AS
+SELECT '{db1_path}' AS observed_in, 
+    get_row_hash(ROW_TO_JSON(t1)::VARCHAR) as hashed_row,
+    {db1_columns} 
+FROM db1.{table} t1;
+
+CREATE OR REPLACE TABLE {table}_t2 AS 
+SELECT '{db2_path}' AS observed_in, 
+    get_row_hash(ROW_TO_JSON(t2)::VARCHAR) as hashed_row,
+    {db2_columns} 
+FROM db2.{table} t2;
+
+CREATE OR REPLACE TABLE {table} AS
+WITH _T1_ONLY_ROWS AS (
+    SELECT _t1.*
+    FROM {table}_t1 _t1
+    ANTI JOIN {table}_t2 _t2 
+    ON _t1.hashed_row = _t2.hashed_row 
+),
+_T2_ONLY_ROWS AS (
+    SELECT _t2.*
+    FROM {table}_t2 _t2
+    ANTI JOIN {table}_t1 _t1
+    ON _t2.hashed_row = _t1.hashed_row 
+)
+SELECT * FROM _T1_ONLY_ROWS
+UNION 
+SELECT * FROM _T2_ONLY_ROWS;"""
+
+
+def compare_table_data(config: Config, table_name: str) -> Union[TableDataDiff, None]:
+    logger.info(f"Comparing table", table=table_name)
+
+    tmp_path = f"/tmp/lotad_config_{table_name}.db"
+    db_interface: LotadConnectionInterface = LotadConnectionInterface.create_connection()
+    tmp_db = db_interface.get_connection(tmp_path, read_only=False)
+
+    db1_path = config.db1_connection_string
+    db1 = db_interface.get_connection(db1_path, read_only=True)
+
+    db2_path = config.db2_connection_string
+    db2 = db_interface.get_connection(db2_path, read_only=True)
+
+    tmp_db.execute(
+        f"""ATTACH '{db1_path}' AS db1 (READ_ONLY);
+        ATTACH '{db2_path}' AS db2 (READ_ONLY);""".lstrip()
+    )
+
+    query = get_gen_table_query(
+        table_name,
+        config,
+        db_interface.get_schema(db1, table_name),
+        db_interface.get_schema(db2, table_name)
+    )
+    db1.close()
+    db2.close()
+
+    try:
+        tmp_db.execute(query)
+        logger.info(f"Successfully processed table", table=table_name)
+        contains_records = tmp_db.execute(
+            f"SELECT * FROM {table_name} LIMIT 1;"
+        ).fetchone()
+        if contains_records:
+            return TableDataDiff(
+                table_name=table_name,
+                tmp_path=tmp_path,
+            )
+        logger.info("No changes discovered", table=table_name)
+    except duckdb.duckdb.CatalogException:
+        logger.warning(f"Failed to process table", table=table_name)
+        return
 
 
 def generate_comparison_report(results: dict, output_path: str = "comparison_report.txt"):
